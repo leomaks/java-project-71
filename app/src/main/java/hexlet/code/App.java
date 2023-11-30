b31431e4c62526b791523745931124246a903013
leomaks <s.arkhin@gmail.com>
2023-11-30 12:58:16 +0200

    add format line

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -21,16 +21,27 @@ import java.util.concurrent.Callable;
 
 public class App implements Runnable {
 
-    @Option(names = {"-V", "--version"}, versionHelp = true, description = "display version info")
+    @Option(names = {"-V", "--version"}, versionHelp = true, description = "Print version information and exit.")
     boolean versionInfoRequested;
 
-    @Option(names = {"-h", "--help"}, usageHelp = true, description = "display this help message")
+    @Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this help message and exit.")
     boolean usageHelpRequested;
+
+    @Option(names = { "-f", "--format" }, paramLabel = "format", description = "output format [default: stylish]")
+    String format;
+    @Parameters(paramLabel = "filepath1", description = "path to first file")
+    File file1;
+    @Parameters(paramLabel = "filepath2", description = "path to second file")
+    File file2;
+
 
     public static void main(String[] args) {
 
+        //String[] args = { "-f", "--file", "file1.txt", "file2.txt" };
+
         CommandLine commandLine = new CommandLine(new App());
         commandLine.parseArgs(args);
+
         if (commandLine.isUsageHelpRequested()) {
             commandLine.usage(System.out);
             return;
