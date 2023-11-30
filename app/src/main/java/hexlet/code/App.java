027a4aa19cd0745915da53aa357d897b7b8a489a
leomaks <s.arkhin@gmail.com>
2023-11-30 12:01:54 +0200

    add help

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,45 @@
 package hexlet.code;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 
-public class App {
+import java.io.File;
+import java.math.BigInteger;
+import java.nio.file.Files;
+import java.security.MessageDigest;
+import java.util.concurrent.Callable;
+
+
+
+@CommandLine.Command(name = "gendiff",
+
+        mixinStandardHelpOptions = true, version = "gendiff 1.1",
+        description = "Compares two configuration files and shows a difference."
+)
+
+
+public class App implements Runnable {
+
+    @Option(names = {"-V", "--version"}, versionHelp = true, description = "display version info")
+    boolean versionInfoRequested;
+
+    @Option(names = {"-h", "--help"}, usageHelp = true, description = "display this help message")
+    boolean usageHelpRequested;
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        CommandLine commandLine = new CommandLine(new App());
+        commandLine.parseArgs(args);
+        if (commandLine.isUsageHelpRequested()) {
+            commandLine.usage(System.out);
+            return;
+        } else if (commandLine.isVersionHelpRequested()) {
+            commandLine.printVersionHelp(System.out);
+            return;
+        }
+
     }
+    @Override
+    public void run() {}
 }
