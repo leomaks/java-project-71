825c83469f9a61df15385e36f5bb9947a1a060f7
leomaks <s.arkhin@gmail.com>
2023-12-01 13:23:27 +0200

    add Differ

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,16 +1,9 @@
 package hexlet.code;
+
 import picocli.CommandLine;
-import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
-
-import java.io.File;
-import java.math.BigInteger;
-import java.nio.file.Files;
-import java.security.MessageDigest;
 import java.util.concurrent.Callable;
-
-
 
 @CommandLine.Command(name = "gendiff",
 
@@ -19,7 +12,7 @@ import java.util.concurrent.Callable;
 )
 
 
-public class App implements Runnable {
+class App implements Callable<Integer> {
 
     @Option(names = {"-V", "--version"}, versionHelp = true, description = "Print version information and exit.")
     boolean versionInfoRequested;
@@ -30,27 +23,24 @@ public class App implements Runnable {
     @Option(names = { "-f", "--format" }, paramLabel = "format", description = "output format [default: stylish]")
     String format;
     @Parameters(paramLabel = "filepath1", description = "path to first file")
-    File file1;
+    static String file1; // = "file1.json";
     @Parameters(paramLabel = "filepath2", description = "path to second file")
-    File file2;
+    static String file2;//  = "file2.json";
 
 
-    public static void main(String[] args) {
 
-        //String[] args = { "-f", "--file", "file1.txt", "file2.txt" };
+    public static void main(String[] args) throws Exception {
 
-        CommandLine commandLine = new CommandLine(new App());
-        commandLine.parseArgs(args);
-
-        if (commandLine.isUsageHelpRequested()) {
-            commandLine.usage(System.out);
-            return;
-        } else if (commandLine.isVersionHelpRequested()) {
-            commandLine.printVersionHelp(System.out);
-            return;
-        }
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
 
     }
+
     @Override
-    public void run() {}
+    public Integer call() throws Exception {
+
+        String list = Differ.generate(file1, file2);
+        System.out.println(list);
+        return null;
+    }
 }
