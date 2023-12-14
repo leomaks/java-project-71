3ad2f9e4ae679e66c4458ed08c0a2323372e041a
leomaks <s.arkhin@gmail.com>
2023-12-14 11:48:24 +0200

    update tests

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -1,94 +1,57 @@
 package hexlet.code;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AppTest {
 
-    private final Path pathPlain = Differ.getPath("./src/test/resources/testPlain.txt");
-    private final Path pathStylish =  Differ.getPath("./src/test/resources/resultStylish.txt");
-    private final Path pathJson =  Differ.getPath("./src/test/resources/resultJson.txt");
+    private static String expectedStylish;
+    private static String expectedPlain;
+    private static String expectedJson;
 
-    private final String filepath1 = "./src/test/resources/file1.json";
-    private final String filepath2 = "./src/test/resources/file2.json";
+    private final String file1 = "./src/test/resources/file1.";
+    private final String file2 = "./src/test/resources/file2.";
 
-    private final String filepathYml1 = "./src/test/resources/file31.yml";
-    private final String filepathYml2 = "./src/test/resources/file32.yml";
 
-    public AppTest() throws Exception {
+    @BeforeAll
+    public static void beforeAll() throws IOException {
+        expectedStylish = Files.readString(Differ.getPath("./src/test/resources/resultStylish.txt"));
+        expectedPlain = Files.readString(Differ.getPath("./src/test/resources/testPlain.txt"));
+        expectedJson = Files.readString(Differ.getPath("./src/test/resources/testJson.txt"));
     }
 
 
-    @Test
-    public void appTestJsonStylish() throws Exception {
 
-        String expected = Files.readString(pathStylish);
-        assertEquals(expected, Differ.generate(filepath1, filepath2, "stylish"));
+    @ParameterizedTest
+    @ValueSource(strings = { "json", "yml" })
+    public void appTestStylish(String ext) throws Exception {
+        assertEquals(expectedStylish, Differ.generate(file1 + ext, file2 + ext, "stylish"));
+    }
 
+    @ParameterizedTest
+    @ValueSource(strings = { "json", "yml" })
+    public void appTestDefault(String ext) throws Exception {
+        assertEquals(expectedStylish, Differ.generate(file1 + ext, file2 + ext));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "json", "yml" })
+    public void appTestPlain(String ext) throws Exception {
+        assertEquals(expectedPlain, Differ.generate(file1 + ext, file2 + ext, "plain"));
     }
 
 
-    @Test
-    public void appTestJsonDefault() throws Exception {
 
-        String expected = Files.readString(pathStylish);
-        assertEquals(expected, Differ.generate(filepath1, filepath2));
-
-    }
-
-    @Test
-    public void appTestJsonPlain() throws Exception {
-
-        String expected = Files.readString(pathPlain);
-        assertEquals(expected, Differ.generate(filepath1, filepath2, "plain"));
-
-    }
-
-
-    @Test
-    public void appTestJsonJson() throws Exception {
-
-        String expected = Files.readString(pathJson);
-        assertEquals(expected, Differ.generate(filepath1, filepath2, "json"));
-
-    }
-
-
-    @Test
-    public void appTestYmlStylish() throws Exception {
-
-        String expected = Files.readString(pathStylish);
-        assertEquals(expected, Differ.generate(filepathYml1, filepathYml2, "stylish"));
-    }
-
-
-    @Test
-    public void appTestYmlDefault() throws Exception {
-
-        String expected = Files.readString(pathStylish);
-        assertEquals(expected, Differ.generate(filepathYml1, filepathYml2));
-
-    }
-
-    @Test
-    public void appTestYmlPlain() throws Exception {
-
-        String expected = Files.readString(pathPlain);
-        assertEquals(expected, Differ.generate(filepathYml1, filepathYml2, "plain"));
-
-    }
-
-
-    @Test
-    public void appTestYmlJson() throws Exception {
-
-        String expected = Files.readString(pathJson);
-        assertEquals(expected, Differ.generate(filepathYml1, filepathYml2, "json"));
-
+    @ParameterizedTest
+    @ValueSource(strings = { "json", "yml" })
+    public void appTestJson(String ext) throws Exception {
+        assertEquals(expectedJson, Differ.generate(file1 + ext, file2 + ext, "json"));
     }
 
 }
